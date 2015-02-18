c332d8d904e38f3c65e332ecba8d8a8c66795318
Jet Rico <jetriconew@gmail.com>
2015-02-18 21:55:10 +0900

    HTML Helper

---
+++ Html.php
@@ -0,0 +1,23 @@
+<?php
+namespace dragonjet\opengraph;
+
+class Html extends \yii\helpers\Html {
+	
+	/*public static function facebookMetaTags($pageData=[]){
+		return '
+			<meta property="og:title" content="'.$pageData['title'].'" />
+			<meta property="og:site_name" content="'.$pageData['title'].'"/>
+			<meta property="og:url" content="'.$pageData['title'].'" />
+			<meta property="og:description" content="'.$pageData['title'].'" />
+			<meta property="og:type" content="article" />
+			<meta property="og:locale" content="'.$pageData['title'].'" />
+			<meta property="og:image" content="'.$pageData['image'].'" />
+		';
+		
+	}*/
+	
+	public static function test(){
+		return 1;
+	}
+	
+}
